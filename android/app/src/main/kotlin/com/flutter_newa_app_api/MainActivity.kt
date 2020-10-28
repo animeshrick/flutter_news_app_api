185e0055d27aac8726da5bb73f754d18486e1ec8
animesh banerjee <43946793+animeshrick@users.noreply.github.com>
2020-10-28 12:22:44 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/flutter_newa_app_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutter_newa_app_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/secret.dart
@@ -0,0 +1,1 @@
+String apiKey = "1a91ca2954f34b1187b8a7b7839e4e5c";
